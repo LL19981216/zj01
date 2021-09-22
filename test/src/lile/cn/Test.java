1f56d44b1c32f72fdf085ffbabb8d1b9b9a93011
LL <1445298556@qq.com>
2021-09-22 19:15:00 +0800

    Initial commit

---
+++ test/src/lile/cn/Test.java
@@ -0,0 +1,12 @@
+package lile.cn;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("我是你爹");
+                System.out.println("我是你爹");
+        System.out.println("我是你爹");
+        System.out.println("我是你爹");
+        System.out.println("我是你爹");
+
+    }
+}
